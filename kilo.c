53192a772a0c01e39b49f7fd063afb2d366fc2b6
Tushar98923 <ty8305@gmail.com>
2026-07-15 12:34:45 +0530

    Implemented error handling

--- kilo.c
+++ kilo.c
@@ -3,14 +3,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
+
 struct termios orig_termios;
 
+void die(const char *s){
+	perror(s);
+	exit(1);
+}
 void disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+		die("tcsetattr");
+	}
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if(tcgetattr(STDIN_FILENO, &orig_termios)){
+  	die("tcsetattr");
+  }
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -29,16 +39,15 @@ int main(){
 	while(1){
 		char c = '\0';
 		read(STDIN_FILENO,&c,1);
-		while (read(STDIN_FILENO, &c ,1) == 1 && c != 'q'){
-			if(iscntrl(c)){
-				printf("%d\r\n", c);
-			}else{
-				printf("%d('%c')\r\n",c,c);
-			}
+		if (read(STDIN_FILENO, &c ,1) == 1 && errno != EAGAIN) die("read");
+		if(iscntrl(c)){
+			printf("%d\r\n", c);
+		}else{
+			printf("%d('%c')\r\n",c,c);
 		}
 		if(c == 'q'){
 			break;
 		}
-	}
+ 	}
 	return 0;
 }
